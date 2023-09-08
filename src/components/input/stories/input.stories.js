de5c00878d0124783a4a32c7b2cb54ceacf9a2b2
Dan Büschlen <danbueschlen@gmail.com>
2023-09-08 16:33:23 +0200

    adjust input story to the actual properties

--- src/components/input/stories/input.stories.js
+++ src/components/input/stories/input.stories.js
@@ -6,30 +6,67 @@ export default {
   component: "leu-input",
   argTypes: {
     identifier: {},
+    name: {},
     label: { control: "text", defaultValue: "Label" },
     value: { control: "text" },
+    pattern: { control: "text" },
+    prefix: { control: "text" },
+    suffix: { control: "text" },
     disabled: { control: "boolean", defaultValue: false },
+    required: { control: "boolean", defaultValue: false },
+    clearable: { control: "boolean", defaultValue: false },
+    invalid: { control: "boolean", defaultValue: false },
   },
 }
 
 function Template({
-  label = "",
+  label = "Label",
   value = "",
-  checked = false,
+  pattern = "",
+  prefix = "",
+  suffix = "",
   disabled = false,
+  required = false,
+  clearable = false,
+  invalid = false,
 }) {
   return html`
-    <leu-input .value=${value} ?checked=${checked} ?disabled=${disabled}>
+    <leu-input
+      .value=${value}
+      pattern=${pattern}
+      prefix=${prefix}
+      suffix=${suffix}
+      ?disabled=${disabled}
+      ?required=${required}
+      ?clearable=${clearable}
+      ?invalid=${invalid}
+    >
       ${label}
     </leu-input>
   `
 }
 
 export const Regular = Template.bind({})
+Regular.args = {
+  label: "Vorname",
+}
 
-export const Checked = Template.bind({})
-Checked.args = {
-  checked: true,
+export const Filled = Template.bind({})
+Filled.args = {
+  label: "Name",
+  value: "Andrea Hugentobler",
+}
+
+export const Prefixed = Template.bind({})
+Filled.args = {
+  label: "Preis",
+  prefix: "CHF",
+}
+
+export const Suffixed = Template.bind({})
+Filled.args = {
+  label: "Länge",
+  suffix: "cm",
 }
 
 export const Disabled = Template.bind({})
@@ -37,8 +74,16 @@ Disabled.args = {
   disabled: true,
 }
 
-export const CheckedDisabled = Template.bind({})
-CheckedDisabled.args = {
-  checked: true,
+export const FilledDisabled = Template.bind({})
+FilledDisabled.args = {
   disabled: true,
+  label: "Name",
+  value: "Andrea Hugentobler",
+}
+
+export const FilledInvalid = Template.bind({})
+FilledInvalid.args = {
+  invalid: true,
+  label: "Name",
+  value: "Andrea Hugentobler",
 }
