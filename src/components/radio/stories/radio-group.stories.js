1333d23567a7eaf73a528fd33954a70db7bd581e
Dan Büschlen <danbueschlen@gmail.com>
2023-09-08 17:35:33 +0200

    handle radio value change

--- src/components/radio/stories/radio-group.stories.js
+++ src/components/radio/stories/radio-group.stories.js
@@ -15,9 +15,9 @@ function Template({ legend = "" }) {
   return html`
     <leu-radio-group>
       <span slot="legend">${legend}</span>
-      <leu-radio identifier="1"></leu-radio>
-      <leu-radio identifier="2">Label 2</leu-radio>
-      <leu-radio identifier="3">Label 3</leu-radio>
+      <leu-radio identifier="1" value="1">Label 1</leu-radio>
+      <leu-radio identifier="2" value="2">Label 2</leu-radio>
+      <leu-radio identifier="3" value="3">Label 3</leu-radio>
     </leu-radio-group>
   `
 }
