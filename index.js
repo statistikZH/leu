64c7175106c6dd91f9f2d11d37b298d44b9f4da4
Dan Büschlen <dab@feinheit.ch>
2023-10-04 17:18:32 +0200

    build: define all files that should be exported

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+export * from "./src/components/checkbox/Checkbox.js"
+export * from "./src/components/checkbox/CheckboxGroup.js"
+export * from "./src/components/input/Input.js"
 export * from "./src/components/radio/Radio.js"
 export * from "./src/components/radio/RadioGroup.js"
+export * from "./src/components/select/Select.js"
+export * from "./src/components/table/Table.js"
--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,20 @@ import postcssLit from "rollup-plugin-postcss-lit"
 export default {
   input: [
     "index.js",
+    "./src/components/checkbox/leu-checkbox.js",
+    "./src/components/checkbox/leu-checkbox-group.js",
+    "./src/components/checkbox/Checkbox.js",
+    "./src/components/checkbox/CheckboxGroup.js",
+    "./src/components/input/leu-input.js",
+    "./src/components/input/Input.js",
     "./src/components/radio/leu-radio.js",
     "./src/components/radio/leu-radio-group.js",
     "./src/components/radio/Radio.js",
     "./src/components/radio/RadioGroup.js",
+    "./src/components/select/leu-select.js",
+    "./src/components/select/Select.js",
+    "./src/components/table/leu-table.js",
+    "./src/components/table/Table.js",
   ],
   output: {
     dir: "./dist/",
