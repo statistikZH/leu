45cd27e8506255bd274b9a33db4f6de36cfcd299
Dan Büschlen <dab@feinheit.ch>
2023-10-05 11:26:14 +0200

    style: configure stylelint

---
+++ stylelint.config.mjs
@@ -0,0 +1,20 @@
+export default {
+  extends: "stylelint-config-standard",
+  overrides: [
+    {
+      files: ["*.js"],
+      customSyntax: "postcss-lit",
+    },
+  ],
+  rules: {
+    // Disable empty line rules because we want to group related properties
+    // and want to separate them with empty lines.
+    "custom-property-empty-line-before": null,
+    "declaration-empty-line-before": null,
+    // Allowing kebab-case and an optional leading underscore
+    "custom-property-pattern": "^_?([a-z][a-z0-9]*)(-[a-z0-9]+)*$",
+    // Allowing kebab-case and BEM
+    "selector-class-pattern":
+      "^[a-z]([-]?[a-z0-9]+)*(__[a-z0-9]([-]?[a-z0-9]+)*)?(--[a-z0-9]([-]?[a-z0-9]+)*)?$",
+  },
+}
