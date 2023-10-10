520ede37fbd21fcc247293449d43990d7caa965c
Dan Büschlen <danbueschlen@gmail.com>
2023-10-10 15:59:46 +0200

    build(postcss): resolve and inline @import rule

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,10 @@
 const postcssPresetEnv = require("postcss-preset-env")
+const atImport = require("postcss-import")
 
 module.exports = {
   stage: 2,
   plugins: [
+    atImport(),
     postcssPresetEnv({
       features: {
         "custom-media-queries": true,
