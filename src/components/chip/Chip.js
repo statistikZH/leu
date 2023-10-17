055082efe3a7006483e1e45f9a8619dfe3628baf
Dan Büschlen <danbueschlen@gmail.com>
2023-10-17 13:45:12 +0200

    docs(chip): add figma link

--- src/components/chip/Chip.js
+++ src/components/chip/Chip.js
@@ -1,6 +1,8 @@
 import { LitElement } from "lit"
 import styles from "./chip.css"
 
+/* Design: https://www.figma.com/file/d6Pv21UVUbnBs3AdcZijHmbN/KTZH-Design-System?type=design&node-id=21161-184433&mode=design&t=Kjo5VDiqivihn8dh-11 */
+
 export class LeuChipBase extends LitElement {
   static styles = styles
 
