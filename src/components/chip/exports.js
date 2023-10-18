6620d2123337e24d2f32894ddd73347cc2ec0e80
Dan Büschlen <dab@feinheit.ch>
2023-10-18 13:48:09 +0200

    build(chip): export chip components and define functions

---
+++ src/components/chip/exports.js
@@ -0,0 +1,10 @@
+export { LeuChipGroup, defineChipGroupElements } from "./ChipGroup.js"
+export { LeuChipLink, defineChipLinkElements } from "./ChipLink.js"
+export {
+  LeuChipRemovable,
+  defineChipRemovableElements,
+} from "./ChipRemovable.js"
+export {
+  LeuChipSelectable,
+  defineChipSelectableElements,
+} from "./ChipSelectable.js"
