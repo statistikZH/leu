971cb4bc2f863172fbfc29ec92526a1e3ca5169a
Dan Büschlen <danbueschlen@gmail.com>
2023-10-18 10:31:46 +0200

    feat(chip): Implement chip components

---
+++ src/components/chip/ChipLink.js
@@ -0,0 +1,45 @@
+import { html } from "lit"
+import { defineElement } from "../../lib/defineElement.js"
+
+import { LeuChipBase } from "./Chip.js"
+
+export const SIZES = {
+  regular: "regular",
+  large: "large",
+}
+
+/**
+ * @slot - The content of the chip
+ * @tagname leu-chip-link
+ */
+export class LeuChipLink extends LeuChipBase {
+  static properties = {
+    ...LeuChipBase.properties,
+
+    /**
+     * The size of the chip
+     * @type {keyof typeof SIZES}
+     */
+    size: { type: String },
+
+    href: { type: String, reflect: true },
+  }
+
+  constructor() {
+    super()
+
+    this.inverted = false
+    this.size = SIZES.regular
+    this.href = ""
+  }
+
+  render() {
+    return html`<a href=${this.href} class="button">
+      <span class="label"><slot></slot></span>
+    </a>`
+  }
+}
+
+export function defineChipLinkElements() {
+  defineElement("chip-link", LeuChipLink)
+}
