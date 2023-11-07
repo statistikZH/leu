fd5d170ee2b26e785556c7b5e7e3e65a099a6e13
Michel Binder <37824907+cubmic@users.noreply.github.com>
2023-11-07 11:51:31 +0100

    feat: add button-group (#37)

---
+++ src/components/button-group/ButtonGroup.js
@@ -0,0 +1,63 @@
+import { html, LitElement } from "lit"
+import { defineElement } from "../../lib/defineElement.js"
+import styles from "./button-group.css"
+import { defineButtonElements } from "../button/Button.js"
+
+/**
+ * @tagname leu-button-group
+ */
+export class LeuButtonGroup extends LitElement {
+  static styles = styles
+
+  static properties = {
+    items: { type: Array, reflect: true },
+    value: { type: String, reflect: true },
+  }
+
+  constructor() {
+    super()
+    /** @type {Array} */
+    this.items = []
+    /** @type {string} */
+    this.value = null
+  }
+
+  _setValue(newValue) {
+    this.value = newValue
+
+    this.dispatchEvent(
+      new CustomEvent("input", {
+        bubbles: true,
+        composed: true,
+        detail: { value: newValue },
+      })
+    )
+  }
+
+  render() {
+    return html`
+      <div role="menubar" class="group">
+        ${this.items.map(
+          (item) =>
+            html`
+              <leu-button
+                label=${item}
+                variant=${this.value === item ? "primary" : "secondary"}
+                @click=${() => {
+                  this._setValue(item)
+                }}
+                role="menuitemradio"
+                aria-checked=${this.value === item}
+              >
+              </leu-button>
+            `
+        )}
+      </div>
+    `
+  }
+}
+
+export function defineButtonGroupElements() {
+  defineButtonElements()
+  defineElement("button-group", LeuButtonGroup)
+}
---
+++ src/components/button-group/stories/button-group.stories.js
@@ -0,0 +1,41 @@
+import { html } from "lit"
+import "../leu-button-group.js"
+
+// https://stackoverflow.com/questions/72566428/storybook-angular-how-to-dynamically-update-args-from-the-template
+import { UPDATE_STORY_ARGS } from "@storybook/core-events" // eslint-disable-line
+function updateStorybookArgss(id, args) {
+  const channel = window.__STORYBOOK_ADDONS_CHANNEL__
+  channel.emit(UPDATE_STORY_ARGS, {
+    storyId: id,
+    updatedArgs: args,
+  })
+}
+
+export default {
+  title: "ButtonGroup",
+  component: "leu-button-group",
+}
+
+function Template({ items, value }, { id }) {
+  return html`
+    <leu-button-group
+      .items=${items}
+      .value=${value}
+      @click=${(event) => {
+        updateStorybookArgss(id, {
+          value: event.target.value,
+        })
+      }}
+    >
+    </leu-button-group>
+    <br />
+    <br />
+    <pre>value = '${value}'</pre>
+  `
+}
+
+export const Regular = Template.bind({})
+Regular.args = {
+  items: ["Eins", "Zwei", "Drei"],
+  selected: "Zwei",
+}
