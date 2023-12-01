8f4bd437e8c002318986dce1ea2758f87d16bfe1
Michel Binder <michel.binder@statistik.ji.zh.ch>
2023-12-01 14:45:08 +0100

    docs: add more Example

--- src/components/header/stories/header.stories.js
+++ src/components/header/stories/header.stories.js
@@ -26,16 +26,58 @@ Regular.argTypes = {
 Regular.args = {
   title: "Trinkwasser",
   color: "blue",
-  breadcrumb: [{ label: "test", href: "www.google.ch" }],
-  topTopics: [
-    { label: "Trinkwasser", href: "www.google.ch" },
-    { label: "Bewilligungen Gesundheitsberufe", href: "www.google.ch" },
-    { label: "Übertragbare Krankheiten", href: "www.google.ch" },
-    { label: "Spitalplanung 2023", href: "www.google.ch" },
+  breadcrumb: [
+    { label: "Kanton Zürich", href: "https://www.zh.ch/de.html" },
+    { label: "Gesundheit", href: "https://www.zh.ch/de/gesundheit.html" },
+    {
+      label: "Lebensmittel & Gebrauchsgegenstände",
+      href: "https://www.zh.ch/de/gesundheit/lebensmittel-gebrauchsgegenstaende.html",
+    },
+    {
+      label: "Lebensmittel",
+      href: "https://www.zh.ch/de/gesundheit/lebensmittel-gebrauchsgegenstaende/lebensmittel.html",
+    },
+    {
+      label: "Trinkwasser",
+      href: "https://www.zh.ch/de/gesundheit/lebensmittel-gebrauchsgegenstaende/lebensmittel/trinkwasser.html",
+    },
   ],
   subtitle:
     "Im Jahr werden im Kanton Zürich rund 140 Millionen Kubikmeter Trinkwasser verbraucht. Dies entspricht in etwa dem Volumen des Greifensees. Das Trinkwasser kommt zu je 40 Prozent aus dem Zürichsee und aus Grundwasservorkommen. Die restlichen 20 Prozent stammen aus Quellen.",
 }
 Regular.parameters = {
+  layout: "fullscreen",
+}
+
+export const TopTopics = Template.bind({})
+TopTopics.argTypes = {
+  color: { control: "select", options: THEME_COLORS },
+}
+TopTopics.args = {
+  title: "Familie",
+  color: "turquoise",
+  breadcrumb: [
+    { label: "Kanton Zürich", href: "https://www.zh.ch/de.html" },
+    { label: "Familie", href: "https://www.zh.ch/de/familie.html" },
+  ],
+  topTopics: [
+    {
+      label: "Kinder- und Jugendhilfezentren",
+      href: "https://www.zh.ch/de/familie/angebote-fuer-familien-mit-kindern/kinder-und-jugendhilfezentren.html",
+    },
+    {
+      label: "Namensänderung",
+      href: "https://www.zh.ch/de/familie/lebensereignisse/namensaenderung.html",
+    },
+    {
+      label: "Sonderpädagogik",
+      href: "https://www.zh.ch/de/familie/angebote-fuer-familien-mit-kindern/sonderpaedagogik.html",
+    },
+    { label: "Adoption", href: "https://www.zh.ch/de/familie/adoption.html" },
+  ],
+  subtitle:
+    "Haben Sie Fragen zum Familienalltag oder suchen Sie Unterstützung bei familiären Herausforderungen? Möchten Sie mehr über die Geschichte Ihrer Familie erfahren? Oder steht ein wichtiges Ereignis bevor und Sie suchen Informationen, um Administratives zu erledigen? Hier finden Sie alles, was Familien im Kanton Zürich bewegt und was sie benötigen.",
+}
+TopTopics.parameters = {
   layout: "fullscreen",
 }
