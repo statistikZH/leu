3c9e54b0cc0e3e0b009bbaf688d8e6e7c0776280
Dan Büschlen <danbueschlen@gmail.com>
2024-01-10 17:43:10 +0100

    chore: make storybook build work and configure static files

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,13 @@
+import rollupJson from "@rollup/plugin-json"
+import rollupCommonjs from "@rollup/plugin-commonjs"
+
+import { plugins as rollupPlugins } from "../rollup.config.js"
+
 /** @type { import('@web/storybook-framework-web-components').StorybookConfig } */
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: ["@storybook/addon-essentials", "@storybook/addon-links"],
+  staticDirs: ["static"],
   framework: {
     name: "@web/storybook-framework-web-components",
   },
@@ -10,6 +16,16 @@ const config = {
   },
   async wdsFinal(config) {
     config.open = false
+    return config
+  },
+  async rollupFinal(config) {
+    config.plugins = [
+      ...config.plugins,
+      ...rollupPlugins.map((p) => p.plugin(...p.args)),
+      rollupCommonjs(),
+      rollupJson(),
+    ]
+
     return config
   },
 }
