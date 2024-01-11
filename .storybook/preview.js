121f0645fffe01909e76a104bfc6823a4d6c5440
Dan Büschlen <danbueschlen@gmail.com>
2024-01-11 13:26:22 +0100

    docs: use breakpoints of the design system in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -14,6 +14,40 @@ const preview = {
         date: /Date$/i,
       },
     },
+    viewport: {
+      viewports: [
+        {
+          name: "Micro",
+          styles: { width: "320px", height: "568px" },
+          type: "mobile",
+        },
+        {
+          name: "Small",
+          styles: { width: "400px", height: "667px" },
+          type: "mobile",
+        },
+        {
+          name: "Regular",
+          styles: { width: "600px", height: "667px" },
+          type: "tablet",
+        },
+        {
+          name: "Medium",
+          styles: { width: "840px", height: "640px" },
+          type: "desktop",
+        },
+        {
+          name: "Large",
+          styles: { width: "1024px", height: "768px" },
+          type: "desktop",
+        },
+        {
+          name: "Ultra",
+          styles: { width: "1280px", height: "900px" },
+          type: "desktop",
+        },
+      ],
+    },
   },
 }
 
