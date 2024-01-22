cf8e2629e4ab10b22ec29205580b9fd0fe8e5c84
Dan Büschlen <danbueschlen@gmail.com>
2024-01-22 11:54:53 +0100

    refactor(breadcrumb): use menu-item link option

--- src/components/breadcrumb/Breadcrumb.js
+++ src/components/breadcrumb/Breadcrumb.js
@@ -168,9 +168,10 @@ export class LeuBreadcrumb extends LitElement {
                 ${this._menuItems.map(
                   (item) =>
                     html`
-                      <a href=${item.href} tabindex="0">
-                        <leu-menu-item label=${item.label}></leu-menu-item>
-                      </a>
+                      <leu-menu-item
+                        label=${item.label}
+                        href=${item.href}
+                      ></leu-menu-item>
                     `
                 )}
               </leu-menu>
