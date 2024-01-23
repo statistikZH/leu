a5c6d182adae8ee2b86dcdbe2a5862f6a762c127
Dan Büschlen <danbueschlen@gmail.com>
2024-01-23 13:08:52 +0100

    docs: invert the color of the description text
    docs: use a more sensible name for the button overview story

--- src/components/button/stories/button.stories.js
+++ src/components/button/stories/button.stories.js
@@ -81,7 +81,7 @@ function Template({
     </style>
     <div
       style="${inverted
-        ? "background:var(--leu-color-accent-blue);"
+        ? "background:var(--leu-color-accent-blue); color: var(--leu-color-white-transp-90);"
         : ""}padding:40px;"
     >
       ${component}
@@ -193,7 +193,7 @@ const groups = [
   },
 ]
 
-function TemplateDev() {
+function TemplateOverview() {
   return html` <style>
       .codeblock {
         position: relative;
@@ -324,8 +324,8 @@ function TemplateDev() {
     )}`
 }
 
-export const Dev = TemplateDev.bind({})
-Dev.argTypes = {
+export const Overview = TemplateOverview.bind({})
+Overview.argTypes = {
   label: { table: { disable: true } },
   icon: { table: { disable: true } },
   iconAfter: { table: { disable: true } },
