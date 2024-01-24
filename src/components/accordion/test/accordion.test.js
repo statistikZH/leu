beea23d2de39ce4ab6821d3a4e9c80a400ed6bd3
Dan Büschlen <danbueschlen@gmail.com>
2024-01-24 14:44:17 +0100

    test: define accordion tests

--- src/components/accordion/test/accordion.test.js
+++ src/components/accordion/test/accordion.test.js
@@ -4,7 +4,9 @@ import { fixture, expect } from "@open-wc/testing"
 import "../leu-accordion.js"
 
 async function defaultFixture() {
-  return fixture(html` <leu-accordion /> `)
+  return fixture(
+    html` <leu-accordion label="Titel des Akkordeons"></leu-accordion> `
+  )
 }
 
 describe("LeuAccordion", () => {
@@ -18,5 +20,93 @@ describe("LeuAccordion", () => {
     const el = await defaultFixture()
 
     await expect(el).shadowDom.to.be.accessible()
+  })
+
+  it("renders the correct heading tag", async () => {
+    const el = await defaultFixture()
+
+    await expect(el._getHeadingTag()).to.equal("h2")
+
+    el.headingLevel = 1
+    await expect(el._getHeadingTag()).to.equal("h1")
+
+    el.headingLevel = 2
+    await expect(el._getHeadingTag()).to.equal("h2")
+
+    el.headingLevel = 3
+    await expect(el._getHeadingTag()).to.equal("h3")
+
+    el.headingLevel = 4
+    await expect(el._getHeadingTag()).to.equal("h4")
+
+    el.headingLevel = 5
+    await expect(el._getHeadingTag()).to.equal("h5")
+
+    el.headingLevel = 6
+    await expect(el._getHeadingTag()).to.equal("h6")
+  })
+
+  it("doesn't render an invalid heading tag", async () => {
+    const el = await defaultFixture()
+
+    el.headingLevel = 10
+    expect(el._getHeadingTag()).to.equal("h2")
+
+    el.headingLevel = -1
+    expect(el._getHeadingTag()).to.equal("h2")
+
+    el.headingLevel = 0
+    expect(el._getHeadingTag()).to.equal("h2")
+
+    el.headingLevel = "h3"
+    expect(el._getHeadingTag()).to.equal("h2")
+  })
+
+  it("toggles the open state", async () => {
+    const el = await fixture(
+      html`<leu-accordion label="Titel des Akkordeons"
+        ><div slot="content">Das ist der Inhalt</div></leu-accordion
+      >`
+    )
+
+    const button = el.shadowRoot.querySelector("button")
+
+    expect(el.open).to.be.false
+
+    button.click()
+    expect(el.open).to.be.true
+
+    button.click()
+    expect(el.open).to.be.false
+  })
+
+  it("doesn't show the content when it is closed", async () => {
+    const el = await fixture(
+      html`<leu-accordion label="Titel des Akkordeons"
+        ><div slot="content">Das ist der Inhalt</div></leu-accordion
+      >`
+    )
+
+    const button = el.shadowRoot.querySelector("button")
+    const content = el.shadowRoot.querySelector(".content")
+
+    expect(content).not.to.be.visible
+
+    button.click()
+    expect(content).to.be.visible
+  })
+
+  it("shows the label prefix before the label", async () => {
+    const el = await fixture(
+      html`<leu-accordion
+        label-prefix="01"
+        label="Titel des Akkordeons"
+      ></leu-accordion>`
+    )
+
+    const button = el.shadowRoot.querySelector("button")
+
+    expect(button).to.contain.text("01")
+    expect(button).to.contain.text("Titel des Akkordeons")
   })
 })
