2836da86ddd2ae43c3d75d692b0e9881c017a309
Dan Büschlen <danbueschlen@gmail.com>
2024-02-13 17:03:11 +0100

    refactor(breadcrumb): rewrite calculation and add some documentation

--- src/components/breadcrumb/Breadcrumb.js
+++ src/components/breadcrumb/Breadcrumb.js
@@ -5,10 +5,36 @@ import styles from "./breadcrumb.css"
 import { Icon } from "../icon/icon.js"
 import "../menu/leu-menu.js"
 import "../menu/leu-menu-item.js"
-// import { debounce } from "../../lib/utils.js"
+import { debounce } from "../../lib/utils.js"
 
 /**
  * A Breadcrumb Navigation.
+ *
+ * The breadcrumbs can be displayed in two different layouts.
+ * Only the back link (the last item / parent of the current page)
+ * is displayed when…
+ * -  … the width of the container is smaller
+ *      than the BACK_ONLY_BREAKPOINT.
+ * -  … less then two breadcrumb items could be displayed
+ *      without overflowing the container.
+ *
+ * Otherwise as many items as possible are displayed in an inline list
+ * without overflowing the container. The remaining items are displayed
+ * in a dropdown menu.
+ *
+ * In order to determine the exact numbers of items that have to be
+ * hidden inside the dropdown, all of them have to be rendered first.
+ * 1. Render all items
+ * 2. Calculate (measure) the number of items that can be displayed
+ *    without overflowing the container.
+ * 3. Updating the state (_hiddeItems) which will trigger a rerender
+ * 4. Render the items again with the new state.
+ *
+ * This results in multiple updates scheduled one after another. Lit
+ * will also print a waring in the console beacause of that.
+ * It's no a nice behaviour but the only one that works without
+ * having duplicate and hidden markup to derive the sizes from that.
+ *
  *
  * @prop {Array} items - Object array with { label, href }
  * @prop {Boolean} inverted - invert color on dark background
@@ -24,15 +50,17 @@ export class LeuBreadcrumb extends LitElement {
 
     _hiddenItems: { state: true },
     _showBackOnly: { state: true },
+    _isRecalculating: { state: true },
+    _isDropdownOpen: { state: true },
   }
 
   static BACK_ONLY_BREAKPOINT = 100
 
   constructor() {
     super()
-    /** @type {array} */
+    /** @type {Array} */
     this.items = []
-    /** @type {boolean} - will be used on dark Background */
+    /** @type {Boolean} - will be used on dark Background */
     this.inverted = false
 
     /** @internal */
@@ -45,25 +73,31 @@ export class LeuBreadcrumb extends LitElement {
     this._showBackOnly = null
     /** @internal */
     this._lastContainerWidth = null
+    /**
+     * @internal
+     * Forces the toggle button to be rendered
+     * so that all possible inline items will be measured.
+     * */
+    this._isRecalculating = true
+    /** @internal */
+    this._isDropdownOpen = false
 
-    this.resizeObserver = new ResizeObserver(() => {
-      this._checkWidth()
-    })
+    this.resizeObserver = new ResizeObserver(
+      debounce(() => {
+        this._handleResize()
+      }, 500)
+    )
   }
 
   firstUpdated() {
     this.resizeObserver.observe(this._containerRef.value)
   }
 
-  updated(changedProperties) {
+  async updated(changedProperties) {
     if (changedProperties.has("items")) {
-      this._checkWidth()
-    }
-
-    if (
-      changedProperties.has("_hiddenItems") &&
-      changedProperties.get("_hiddenItems") > this._hiddenItems
-    ) {
+      this._hiddenItems = 0
+      this._isRecalculating = true
+      await this.updateComplete
       this._checkWidth()
     }
   }
@@ -71,14 +105,7 @@ export class LeuBreadcrumb extends LitElement {
   disconnectedCallback() {
     super.disconnectedCallback()
 
-    if (
-      this._dropdownRef &&
-      this._dropdownRef.value &&
-      this._dropdownRef.value.classList.contains("show")
-    ) {
-      this._closeDropdown()
-    }
-
+    window.removeEventListener("click", this._closeDropdown)
     this.resizeObserver.disconnect()
   }
 
@@ -92,34 +119,66 @@ export class LeuBreadcrumb extends LitElement {
     return this.items.slice(1, 1 + this._hiddenItems)
   }
 
+  _handleResize = async () => {
+    const containerOffsetWidth = this._containerRef.value.offsetWidth
+    const sizeIsGrowing = containerOffsetWidth > this._lastContainerWidth
+    this._lastContainerWidth = containerOffsetWidth
+
+    /**
+     * Show only the back link (parent of the current page)
+     * when the width of the container is smaller than the BACK_ONLY_BREAKPOINT
+     */
+    if (containerOffsetWidth <= LeuBreadcrumb.BACK_ONLY_BREAKPOINT) {
+      this._showBackOnly = true
+      this._isRecalculating = false
+      return
+    }
+
+    /**
+     *  In order to calculate how many items can be displayed
+     *  when the container is growing, all items have to
+     *  be marked as displayed (_hiddenItems = 0) and
+     *  rendered.
+     */
+    if (sizeIsGrowing && this._hiddenItems > 0) {
+      this._hiddenItems = 0
+      this._isRecalculating = true
+      await this.updateComplete
+    }
+
+    this._checkWidth()
+  }
+
+  /**
+   * Calculate the number of items that can be displayed
+   * without overflowing the container.
+   * @internal
+   * @returns {void}
+   */
   _checkWidth() {
     const containerOffsetWidth = this._containerRef.value.offsetWidth
     const containerScrollWidth = this._containerRef.value.scrollWidth
-
-    const sizeIsGrowing = containerOffsetWidth > this._lastContainerWidth
-
     this._lastContainerWidth = containerOffsetWidth
 
-    if (containerOffsetWidth <= LeuBreadcrumb.BACK_ONLY_BREAKPOINT) {
-      this._showBackOnly = true
+    /** When the container is not overflowing, nothing has to be done */
+    if (containerOffsetWidth === containerScrollWidth) {
+      this._isRecalculating = false
       return
     }
 
-    this._showBackOnly = false
-
-    if (sizeIsGrowing) {
-      this._hiddenItems = 0
-      return
-    }
-
-    if (containerOffsetWidth === containerScrollWidth) return
-
-    const listItems = this._containerRef.value.querySelectorAll("li")
+    const listItems = this._containerRef.value.querySelectorAll(
+      "li:not([data-dropdown-toggle])"
+    )
     const listItemWidths = [...listItems].map((o) => o.offsetWidth)
 
     let hiddenItems = 0
     let nextItemWidthSum = containerScrollWidth
 
+    /**
+     * Remove item by item until the sum of the remaining items
+     * is smaller than the width of the container.
+     * The first item will not be removed.
+     */
     while (
       hiddenItems < listItemWidths.length &&
       containerOffsetWidth < nextItemWidthSum
@@ -132,25 +191,20 @@ export class LeuBreadcrumb extends LitElement {
     }
 
     this._hiddenItems += hiddenItems
+    this._isRecalculating = false
   }
 
   /** @internal */
   _openDropdown = (e) => {
-    // arrow function to use this in event listener
-    if (e) {
-      e.stopPropagation()
-    }
-    this._dropdownRef.value.classList.add("show")
+    e.stopPropagation()
+    this._isDropdownOpen = true
     window.addEventListener("click", this._closeDropdown)
   }
 
   /** @internal */
   _closeDropdown = (e) => {
-    // arrow function to use this in event listener
-    if (e) {
-      e.stopPropagation()
-    }
-    this._dropdownRef.value.classList.remove("show")
+    e.stopPropagation()
+    this._isDropdownOpen = false
     window.removeEventListener("click", this._closeDropdown)
   }
 
@@ -159,30 +213,33 @@ export class LeuBreadcrumb extends LitElement {
    * @returns
    */
   renderDropdown() {
-    if (this._dropdownItems.length === 0) return nothing
+    if (this._dropdownItems.length === 0 && !this._isRecalculating)
+      return nothing
 
     return html`
-      <li>
+      <li data-dropdown-toggle>
         <span>${Icon("angleRight")}</span>
         <div class="dropdown">
           <button class="menu" @click=${this._openDropdown} tabindex="0">
             &hellip;
           </button>
-          <div ref=${ref(this._dropdownRef)} class="dropdown-content">
-            ${html`
-              <leu-menu>
-                ${this._dropdownItems.map(
-                  (item) =>
-                    html`
-                      <leu-menu-item
-                        label=${item.label}
-                        href=${item.href}
-                      ></leu-menu-item>
-                    `
-                )}
-              </leu-menu>
-            `}
-          </div>
+          ${this._isDropdownOpen
+            ? html`<div ref=${ref(this._dropdownRef)} class="dropdown-content">
+                ${html`
+                  <leu-menu>
+                    ${this._dropdownItems.map(
+                      (item) =>
+                        html`
+                          <leu-menu-item
+                            label=${item.label}
+                            href=${item.href}
+                          ></leu-menu-item>
+                        `
+                    )}
+                  </leu-menu>
+                `}
+              </div>`
+            : nothing}
         </div>
       </li>
     `
@@ -209,10 +266,10 @@ export class LeuBreadcrumb extends LitElement {
                   html`
                     <li>
                       ${index > 0
-                        ? html`<span>${Icon("angleRight")}</span>` // First list item should not have an arrow
+                        ? html`<span>${Icon("angleRight")}</span>` // First list item doesn't have an arrow
                         : nothing}
                       ${index === list.length - 1
-                        ? item.label // Last list item should not be a link
+                        ? item.label // Last list item doesn't contain a link
                         : html`<a href=${item.href}>${item.label}</a>`}
                     </li>
                     ${index === 0 ? this.renderDropdown() : nothing}
