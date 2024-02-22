a0390322a6e30b60931fdbf6c96f287951ebc28a
Dan Büschlen <danbueschlen@gmail.com>
2024-02-22 14:50:39 +0100

    docs: make storybook argTypes inferring work again (#139)

---
+++ custom-elements-manifest.config.js
@@ -0,0 +1,7 @@
+export default {
+  globs: ["src/**/*.js"],
+  exclude: ["src/**/test/**/*.js", "src/**/stories/**/*.js"],
+  outdir: ".",
+  /** Enable special handling for litelement */
+  litelement: true,
+}
