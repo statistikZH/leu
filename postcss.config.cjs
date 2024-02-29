3d42d8ff9c9e50c6d9cafd391eb90252a3854d2b
Dan Büschlen <dab@feinheit.ch>
2024-02-29 16:33:56 +0100

    refactor(theme): rewrite font style generator as a postcss plugin

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,12 @@
 const postcssPresetEnv = require("postcss-preset-env")
 const atImport = require("postcss-import")
+const leuFontStyles = require("./scripts/postcss-leu-font-styles.cjs")
 
 module.exports = {
   stage: 2,
   plugins: [
     atImport(),
+    leuFontStyles(),
     postcssPresetEnv({
       features: {
         "custom-media-queries": true,
