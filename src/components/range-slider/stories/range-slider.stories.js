4bab89fa92c5e110cd6ae4b7beed726b61a5bb0a
Res Marti <83967227+resmartiZH@users.noreply.github.com>
2024-03-04 09:57:03 +0000

    feat: added option for dates and months and added sample to story

--- src/components/range-slider/stories/range-slider.stories.js
+++ src/components/range-slider/stories/range-slider.stories.js
@@ -106,3 +106,36 @@ Step.args = {
   toValue: 50,
   step: 0.1,
 }
+
+export const Dates = Template.bind({})
+Dates.args = {
+  label: "Wert auswählen",
+  min: new Date("2010.01.01").getTime(),
+  max: new Date("2014.01.01").getTime(),
+  fromValue: new Date("2013.01.01").getTime(),
+  toValue: new Date("2013.02.01").getTime(),
+  labelsFormat: {
+    locale: "de-CH",
+    options: {
+      style: "date",
+      unit: "day",
+    },
+  },
+}
+
+export const Months = Template.bind({})
+Months.args = {
+  label: "Wert auswählen",
+  min: new Date(Date.UTC(2020, 1 - 1, 15)).getTime(),
+  max: new Date(Date.UTC(2024, 3 - 1, 15)).getTime(),
+  fromValue: new Date(Date.UTC(2023, 1 - 1, 15)).getTime(),
+  toValue: new Date(Date.UTC(2024, 3 - 1, 15)).getTime(),
+  labelsFormat: {
+    locale: "de-CH",
+    options: {
+      style: "date",
+      unit: "month",
+    },
+  },
+  step: (86400000 * 365) / 12,
+}
