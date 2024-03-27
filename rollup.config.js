e2896e43d37afd2a6d6623be03d711e4742cc3e5
Dan Büschlen <dab@feinheit.ch>
2024-03-27 17:46:19 +0100

    build: replace hardcoded paths with globbing

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,6 @@
+import { globSync } from "glob"
+import path from "path"
+import { fileURLToPath } from "url"
 import postcss from "rollup-plugin-postcss"
 import postcssLit from "rollup-plugin-postcss-lit"
 import { babel } from "@rollup/plugin-babel"
@@ -17,44 +20,25 @@ export const plugins = [
   },
 ]
 
+/**
+ * @type {import("rollup").RollupOptions}
+ */
 export default {
-  input: [
-    "index.js",
-    "./src/components/button/leu-button.js",
-    "./src/components/button/Button.js",
-    "./src/components/button-group/leu-button-group.js",
-    "./src/components/button-group/ButtonGroup.js",
-    "./src/components/checkbox/leu-checkbox.js",
-    "./src/components/checkbox/leu-checkbox-group.js",
-    "./src/components/checkbox/Checkbox.js",
-    "./src/components/checkbox/CheckboxGroup.js",
-    "./src/components/chip/leu-chip-group.js",
-    "./src/components/chip/leu-chip-link.js",
-    "./src/components/chip/leu-chip-removable.js",
-    "./src/components/chip/leu-chip-selectable.js",
-    "./src/components/chip/ChipGroup.js",
-    "./src/components/chip/ChipLink.js",
-    "./src/components/chip/ChipRemovable.js",
-    "./src/components/chip/ChipSelectable.js",
-    "./src/components/dropdown/leu-dropdown.js",
-    "./src/components/dropdown/Dropdown.js",
-    "./src/components/input/leu-input.js",
-    "./src/components/input/Input.js",
-    "./src/components/menu/leu-menu.js",
-    "./src/components/menu/leu-menu-item.js",
-    "./src/components/menu/Menu.js",
-    "./src/components/menu/MenuItem.js",
-    "./src/components/pagination/leu-pagination.js",
-    "./src/components/pagination/Pagination.js",
-    "./src/components/radio/leu-radio.js",
-    "./src/components/radio/leu-radio-group.js",
-    "./src/components/radio/Radio.js",
-    "./src/components/radio/RadioGroup.js",
-    "./src/components/select/leu-select.js",
-    "./src/components/select/Select.js",
-    "./src/components/table/leu-table.js",
-    "./src/components/table/Table.js",
-  ],
+  // Select all files in a direct subdirectory of src/components
+  // that have a name starting with
+  // -  a capital letter
+  // -  or "leu-"
+  input: {
+    "index.js": "index.js",
+    ...Object.fromEntries(
+      globSync("src/components/*/{[A-Z],leu-}*.js", { nocase: false }).map(
+        (file) => [
+          path.basename(file, path.extname(file)),
+          fileURLToPath(new URL(file, import.meta.url)),
+        ]
+      )
+    ),
+  },
   output: {
     dir: "./dist/",
     format: "esm",
@@ -66,6 +50,3 @@ export default {
   ],
   external: /^lit(\/.*\.js)?$/,
 }
-
-// TODO: add a second config for a bundle with esbuild
-// import esbuild from "rollup-plugin-esbuild"
