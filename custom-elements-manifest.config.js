0c97af0254041702436990682c56b9bc2d7d87de
Dan Büschlen <danbueschlen@gmail.com>
2024-04-03 10:06:51 +0200

    build: export meta information about components for code completion in editors (#163)

--- custom-elements-manifest.config.js
+++ custom-elements-manifest.config.js
@@ -1,7 +1,46 @@
+import { customElementJetBrainsPlugin } from "custom-element-jet-brains-integration"
+import { customElementVsCodePlugin } from "custom-element-vs-code-integration"
+import { customElementVuejsPlugin } from "custom-element-vuejs-integration"
+
 export default {
-  globs: ["src/**/*.js"],
+  globs: ["src/components/*/*.js"],
   exclude: ["src/**/test/**/*.js", "src/**/stories/**/*.js"],
-  outdir: ".",
+  outdir: "./dist",
   /** Enable special handling for litelement */
   litelement: true,
+  plugins: [
+    // Generate custom VS Code data
+    customElementVsCodePlugin({
+      outdir: "./dist",
+      cssFileName: null,
+      referencesTemplate: (_, tag) => [
+        {
+          name: "Documentation",
+          url: `https://statistikzh.github.io/leu/?path=/story/${tag.replace(
+            "leu-",
+            ""
+          )}`,
+        },
+      ],
+    }),
+
+    customElementJetBrainsPlugin({
+      outdir: "./dist",
+      excludeCss: true,
+      packageJson: false,
+      referencesTemplate: (_, tag) => ({
+        name: "Documentation",
+        url: `https://statistikzh.github.io/leu/?path=/story/${tag.replace(
+          "leu-",
+          ""
+        )}`,
+      }),
+    }),
+
+    customElementVuejsPlugin({
+      outdir: "./dist/vue",
+      fileName: "index.d.ts",
+      componentTypePath: (name) => `../${name.replace("Leu", "")}.js`,
+    }),
+  ],
 }
