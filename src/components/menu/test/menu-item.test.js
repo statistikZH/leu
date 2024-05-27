bb2a7ddc2517c9c1bb7978dd859016a68b984172
Dan Büschlen <dab@feinheit.ch>
2024-05-27 16:49:41 +0200

    refactor(menu-item): use slots instead of attributes to display icons and the label

--- src/components/menu/test/menu-item.test.js
+++ src/components/menu/test/menu-item.test.js
@@ -8,13 +8,12 @@ import "../leu-menu-item.js"
 async function defaultFixture(args = {}) {
   return fixture(html`
     <leu-menu-item
-      label=${args.label}
-      before=${ifDefined(args.before)}
-      after=${ifDefined(args.after)}
       href=${ifDefined(args.href)}
       ?active=${args.active}
       ?disabled=${args.disabled}
-    ></leu-menu-item>
+    >
+      ${args.label}
+    </leu-menu-item>
   `)
 }
 
@@ -43,9 +42,7 @@ describe("LeuMenuItem", () => {
   it("renders a label", async () => {
     const el = await defaultFixture({ label: "Download" })
 
-    const button = el.shadowRoot.querySelector("button")
-
-    expect(button).to.have.trimmed.text("Download")
+    expect(el).to.have.trimmed.text("Download")
   })
 
   it("renders a button", async () => {
@@ -66,83 +63,7 @@ describe("LeuMenuItem", () => {
 
     expect(link).to.exist
     expect(link).to.have.attribute("href", "https://zh.ch")
-    expect(link).to.have.trimmed.text("Kanton Zürich")
-  })
-
-  it("renders a before icon", async () => {
-    const el = await defaultFixture({ label: "Download", before: "download" })
-
-    const before = el.shadowRoot.querySelector(".before")
-    expect(before).to.exist
-
-    expect(el).shadowDom.to.equal(
-      "<button class='button'><span class='before'></span><span class='label'>Download</span></button>"
-    )
-  })
-
-  it("renders a before label", async () => {
-    const el = await defaultFixture({ label: "Download", before: "DE" })
-
-    const before = el.shadowRoot.querySelector(".before")
-    expect(before).to.exist
-    expect(before).to.have.trimmed.text("DE")
-
-    expect(el).shadowDom.to.equal(
-      "<button class='button'><span class='before'>DE</span><span class='label'>Download</span></button>"
-    )
-  })
-
-  it("renders a before placeholder", async () => {
-    const el = await defaultFixture({ label: "Download", before: "EMPTY" })
-
-    const before = el.shadowRoot.querySelector(".before")
-    expect(before).to.exist
-    expect(before).to.not.have.trimmed.text()
-
-    const iconPlaceholder = before.querySelector(".icon-placeholder")
-    expect(iconPlaceholder).to.exist
-
-    expect(el).shadowDom.to.equal(
-      "<button class='button'><span class='before'><div class='icon-placeholder'></div></span><span class='label'>Download</span></button>"
-    )
-  })
-
-  it("renders a after icon", async () => {
-    const el = await defaultFixture({ label: "Download", after: "download" })
-
-    const after = el.shadowRoot.querySelector(".after")
-    expect(after).to.exist
-
-    expect(el).shadowDom.to.equal(
-      "<button class='button'><span class='label'>Download</span><span class='after'></span></button>"
-    )
-  })
-
-  it("renders a after label", async () => {
-    const el = await defaultFixture({ label: "Download", after: "DE" })
-
-    const after = el.shadowRoot.querySelector(".after")
-    expect(after).to.exist
-    expect(after).to.have.trimmed.text("DE")
-
-    expect(el).shadowDom.to.equal(
-      "<button class='button'><span class='label'>Download</span><span class='after'>DE</span></button>"
-    )
-  })
-
-  it("renders a after placeholder", async () => {
-    const el = await defaultFixture({ label: "Download", after: "EMPTY" })
-
-    const after = el.shadowRoot.querySelector(".after")
-    expect(after).to.exist
-    expect(after).to.not.have.trimmed.text()
-
-    const iconPlaceholder = after.querySelector(".icon-placeholder")
-    expect(iconPlaceholder).to.exist
-
-    expect(el).shadowDom.to.equal(
-      "<button class='button'><span class='label'>Download</span><span class='after'><div class='icon-placeholder'></div></span></button>"
-    )
+    expect(el).to.have.trimmed.text("Kanton Zürich")
   })
 
   it("passes the disabled attribute to the button", async () => {
