8793bd99a265604712edbca58759676975630263
Dan Büschlen <danbueschlen@gmail.com>
2024-07-24 15:55:59 +0200

    chore: define preventAssignment as it's default value will change with the next major version

--- rollup.config.js
+++ rollup.config.js
@@ -11,7 +11,10 @@ export const plugins = [
     plugin: replace,
     args: [
       {
-        __LEU_VERSION__: JSON.stringify(process.env.npm_package_version),
+        preventAssignment: true,
+        values: {
+          __LEU_VERSION__: JSON.stringify(process.env.npm_package_version),
+        },
       },
     ],
   },
