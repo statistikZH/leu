805be868585a1ee59a45bf1d7facd0886596906c
Dan Büschlen <danbueschlen@gmail.com>
2024-09-12 09:17:40 +0200

    feat: add dialog component
    ---------
    
    Co-authored-by: Michel Binder <michel.binder@statistik.ji.zh.ch>

---
+++ src/components/dialog/stories/dialog.stories.js
@@ -0,0 +1,144 @@
+import { html } from "lit"
+import "../leu-dialog.js"
+import "../../input/leu-input.js"
+import "../../button/leu-button.js"
+
+export default {
+  title: "Dialog",
+  component: "leu-dialog",
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/d6Pv21UVUbnBs3AdcZijHmbN/KTZH-Design-System?node-id=16052-88818&node-type=canvas&t=swv3JY6UIoCBUpXa-11",
+    },
+  },
+}
+
+function openDialog() {
+  document.querySelector("leu-dialog").show()
+}
+
+function closeDialog() {
+  document.querySelector("leu-dialog").close()
+}
+
+function OpenDialogButton() {
+  return html` <leu-button @click=${() => openDialog()}>
+    Open dialog
+  </leu-button>`
+}
+
+function Template({ label, sublabel }) {
+  return html`
+    ${OpenDialogButton()}
+
+    <leu-dialog label="${label}" sublabel="${sublabel}">
+      <leu-input label="Eingabe"></leu-input>
+      <br />
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+      amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+      nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+      diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
+      sit amet.
+    </leu-dialog>
+  `
+}
+
+export const Regular = Template.bind({})
+Regular.args = {
+  label: "Property: label",
+  rubric: "Property: sublabel",
+}
+
+function ActionButtonsTemplate({ label, sublabel }) {
+  return html`
+    ${OpenDialogButton()}
+
+    <leu-dialog label="${label}" sublabel="${sublabel}">
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+      amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+      nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+      diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
+      sit amet.
+      <slot slot="actionbar">
+        <leu-button
+          variant="secondary"
+          @click=${() => {
+            // eslint-disable-next-line no-alert
+            alert("Fenster wird geschlossen")
+            closeDialog()
+          }}
+        >
+          Abbrechen
+        </leu-button>
+        <leu-button
+          @click=${() => {
+            // eslint-disable-next-line no-alert
+            alert("Fenster wird offen gelassen")
+          }}
+        >
+          Anwenden
+        </leu-button>
+      </slot>
+    </leu-dialog>
+  `
+}
+
+export const ActionButtons = ActionButtonsTemplate.bind({})
+ActionButtons.args = {
+  label: "Property: label",
+  sublabel: "Property: sublabel",
+}
+
+function SmallContentTemplate({ label, sublabel }) {
+  return html`
+    ${OpenDialogButton()}
+
+    <leu-dialog label="${label}" sublabel="${sublabel}">
+      <p>Ein kurzer Text</p>
+    </leu-dialog>
+  `
+}
+
+export const SmallContent = SmallContentTemplate.bind({})
+SmallContent.args = {
+  label: "Dialog Titel",
+  sublabel: "Property: sublabel",
+}
+
+function ScrollablePageTemplate({ label, sublabel }) {
+  return html`
+    <div style="height: 200vh">
+      ${OpenDialogButton()}
+
+      <leu-dialog label="${label}" sublabel="${sublabel}">
+        <leu-input label="Eingabe"></leu-input>
+        <br />
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+        ipsum dolor sit amet.
+      </leu-dialog>
+    </div>
+    <div style="height: 200vh">${OpenDialogButton()}</div>
+  `
+}
+
+export const ScrollablePage = ScrollablePageTemplate.bind({})
+ScrollablePage.args = {
+  label: "Dialog Titel",
+  sublabel: "Property: sublabel",
+}
---
+++ src/components/dialog/test/dialog.test.js
@@ -0,0 +1,85 @@
+import { html } from "lit"
+import { fixture, expect } from "@open-wc/testing"
+
+import "../leu-dialog.js"
+
+async function defaultFixture() {
+  return fixture(
+    html`<leu-dialog label="Title" sublabel="Category">
+      This is the content of the dialog.
+    </leu-dialog>`
+  )
+}
+
+describe("LeuDialog", () => {
+  it("is a defined element", async () => {
+    const el = await customElements.get("leu-dialog")
+
+    expect(el).not.to.be.undefined
+  })
+
+  it("passes the a11y audit", async () => {
+    const el = await defaultFixture()
+
+    await expect(el).shadowDom.to.be.accessible()
+  })
+
+  it("passes the a11y audit when the modal is open", async () => {
+    const el = await defaultFixture()
+
+    el.show()
+
+    await expect(el).shadowDom.to.be.accessible()
+  })
+
+  it("opens the dialog when calling show()", async () => {
+    const el = await defaultFixture()
+
+    el.show()
+
+    const dialog = el.shadowRoot.querySelector("dialog")
+
+    expect(dialog.open).to.be.true
+  })
+
+  it("closes the dialog when calling close()", async () => {
+    const el = await defaultFixture()
+
+    el.show()
+    el.close()
+
+    const dialog = el.shadowRoot.querySelector("dialog")
+
+    expect(dialog.open).to.be.false
+  })
+
+  it("renders a label and a sublabel", async () => {
+    const el = await defaultFixture()
+
+    const label = el.shadowRoot.querySelector("h1")
+    const sublabel = el.shadowRoot.querySelector(".subtitle")
+
+    expect(label).to.have.text("Title")
+    expect(sublabel).to.have.text("Category")
+  })
+
+  it("renders a close button", async () => {
+    const el = await defaultFixture()
+
+    const closeButton = el.shadowRoot.querySelector(".close-button")
+
+    expect(closeButton).to.exist
+  })
+
+  it("closes the dialog when clicking the close button", async () => {
+    const el = await defaultFixture()
+    const dialog = el.shadowRoot.querySelector("dialog")
+
+    el.show()
+
+    const closeButton = el.shadowRoot.querySelector(".close-button")
+    expect(dialog.open).to.be.true
+    closeButton.click()
+    expect(dialog.open).to.be.false
+  })
+})
