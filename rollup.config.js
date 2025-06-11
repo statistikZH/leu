35c1b242c7e4540f61548534215b4a7fd1617e0e
Dan Büschlen <danbueschlen@gmail.com>
2025-06-11 15:38:17 +0000

    build: draft of bundling .d.ts files

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,7 @@ import postcssLit from "rollup-plugin-postcss-lit"
 import { babel } from "@rollup/plugin-babel"
 import replace from "@rollup/plugin-replace"
 import typescript from "rollup-plugin-typescript2"
+import { dts } from "rollup-plugin-dts"
 
 export const plugins = [
   {
@@ -36,31 +37,56 @@ export const plugins = [
 /**
  * @type {import("rollup").RollupOptions}
  */
-export default {
-  // Select all files in a direct subdirectory of src/components
-  // that have a name starting with
-  // -  a capital letter
-  // -  or "leu-"
-  input: {
-    index: "index.ts",
-    ...Object.fromEntries(
-      globSync("src/components/*/{[A-Z],leu-}*.ts", { nocase: false }).map(
-        (file) => [
-          path.basename(file, path.extname(file)),
-          fileURLToPath(new URL(file, import.meta.url)),
-        ],
+export default [
+  {
+    // Select all files in a direct subdirectory of src/components
+    // that have a name starting with
+    // -  a capital letter
+    // -  or "leu-"
+    input: {
+      index: "index.ts",
+      ...Object.fromEntries(
+        globSync("src/components/*/{[A-Z],leu-}*.ts", { nocase: false }).map(
+          (file) => [
+            path.basename(file, path.extname(file)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ],
+        ),
       ),
-    ),
+    },
+    output: {
+      dir: "./dist/",
+      format: "esm",
+      entryFileNames: "[name].js",
+    },
+    plugins: [
+      typescript({
+        tsconfigOverride: {
+          compilerOptions: {
+            emitDeclarationOnly: false,
+            declaration: false,
+          },
+        },
+      }),
+      babel({ babelHelpers: "bundled" }),
+      ...plugins.map((p) => p.plugin(...p.args)),
+    ],
+    external: [/^lit(\/.*\.js)?$/, "@floating-ui/dom"],
   },
-  output: {
-    dir: "./dist/",
-    format: "esm",
-    entryFileNames: "[name].js",
+  {
+    input: {
+      ...Object.fromEntries(
+        globSync("dist/components/*/{[A-Z],leu-}*.d.ts", { nocase: false }).map(
+          (file) => [
+            path.basename(file, ".d.ts"),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ],
+        ),
+      ),
+    },
+    output: {
+      dir: "./dist/",
+    },
+    plugins: [dts()],
   },
-  plugins: [
-    typescript(),
-    babel({ babelHelpers: "bundled" }),
-    ...plugins.map((p) => p.plugin(...p.args)),
-  ],
-  external: [/^lit(\/.*\.js)?$/, "@floating-ui/dom"],
-}
+]
