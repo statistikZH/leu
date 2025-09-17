00042ce99f3a3867337dfc63646af867d809003c
Dan Büschlen <danbueschlen@gmail.com>
2025-09-17 17:00:28 +0200

    chore: remove unnecessary awaits and improve typing

--- scripts/generate-component/templates/test/[name].test.ts
+++ scripts/generate-component/templates/test/[name].test.ts
@@ -2,16 +2,17 @@ import { html } from "lit"
 import { fixture, expect } from "@open-wc/testing"
 
 import "../[namespace]-[name].js"
+import { type [Namespace][Name] } from "../[Name].js"
 
 async function defaultFixture() {
-  return fixture(html`<[namespace]-[name]></[namespace]-[name]>`)
+  return fixture<[Namespace][Name]>(html`<[namespace]-[name]></[namespace]-[name]>`)
 }
 
 describe("[Namespace][Name]", () => {
   it("is a defined element", async () => {
-    const el = await customElements.get("[namespace]-[name]")
+    const el = customElements.get("[namespace]-[name]")
 
-    await expect(el).not.to.be.undefined
+    expect(el).not.to.be.undefined
   })
 
   it("passes the a11y audit", async () => {
